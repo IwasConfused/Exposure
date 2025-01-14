5f1a09ba160006812486b6a16774fe4250d99322
mortuusars <mortuusars@gmail.com>
2025-01-14 19:13:46 +0200

    Bsod adjustments

--- common/src/main/java/io/github/mortuusars/exposure/data/Filters.java
+++ common/src/main/java/io/github/mortuusars/exposure/data/Filters.java
@@ -8,8 +8,6 @@ import java.util.Optional;
 
 public class Filters {
     public static Optional<Filter> of(RegistryAccess registryAccess, ItemStack stack) {
-        if (!stack.is(Exposure.Tags.Items.FILTERS)) return Optional.empty();
-
         return registryAccess.registryOrThrow(Exposure.Registries.FILTER)
                 .stream()
                 .filter(filter -> filter.predicate().test(stack))
