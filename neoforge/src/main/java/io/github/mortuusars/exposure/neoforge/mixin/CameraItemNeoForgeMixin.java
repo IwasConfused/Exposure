ecd46a5607e1013554adea9a1c21231f91009a03
mortuusars <mortuusars@gmail.com>
2025-01-22 06:20:55 +0200

    Fix? reequip animation

--- neoforge/src/main/java/io/github/mortuusars/exposure/neoforge/mixin/CameraItemNeoForgeMixin.java
+++ neoforge/src/main/java/io/github/mortuusars/exposure/neoforge/mixin/CameraItemNeoForgeMixin.java
@@ -3,50 +3,18 @@ package io.github.mortuusars.exposure.neoforge.mixin;
 import io.github.mortuusars.exposure.world.item.CameraItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.UseAnim;
-import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import net.neoforged.neoforge.common.extensions.IItemExtension;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 
-import java.util.function.Consumer;
-
-@Mixin(value = CameraItem.class, remap = false)
+@Mixin(value = CameraItem.class)
 public abstract class CameraItemNeoForgeMixin extends Item implements IItemExtension {
     public CameraItemNeoForgeMixin(Properties properties) {
         super(properties);
     }
 
-//    @Shadow abstract InteractionResult useCamera(Player player, InteractionHand hand);
-
-//    @Override
-//    public @NotNull InteractionResult onItemUseFirst(@NotNull ItemStack stack, UseOnContext context) {
-//        Player player = context.getPlayer();
-//        if (player != null) {
-//            InteractionHand hand = context.getHand();
-//
-//            //TODO: both hands
-////            if (hand == InteractionHand.MAIN_HAND && Camera.getCamera(player)
-////                    .filter(c -> c instanceof CameraInHand<?>)
-////                    .map(c -> ((CameraInHand<?>) c).getHand() == InteractionHand.OFF_HAND).orElse(false)) {
-////                return InteractionResult.PASS;
-////            }
-//
-////            return useCamera(player, hand);
-//        }
-//        return InteractionResult.CONSUME; // To not play attack animation.
-//    }
-
     @Override
     public boolean shouldCauseReequipAnimation(ItemStack oldStack, @NotNull ItemStack newStack, boolean slotChanged) {
-        if (oldStack.getItem() instanceof CameraItem oldItem && newStack.getItem() instanceof CameraItem newItem) {
-            return !oldItem.getOrCreateID(oldStack).equals(newItem.getOrCreateID(newStack));
-        }
-        return super.shouldCauseReequipAnimation(oldStack, newStack, slotChanged);
-    }
-
-    @Override
-    public @NotNull UseAnim getUseAnimation(@NotNull ItemStack stack) {
-        return UseAnim.CUSTOM;
+        return !ItemStack.isSameItemSameComponents(newStack, oldStack);
     }
 }
